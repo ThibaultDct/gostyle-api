ddd56b494bb98fa30164c0a5c0646d06da2a25b7
Thibault Doucet <thibault.doucet44@gmail.com>
2021-01-15 09:23:33 +0100

    :tada: Initial commit

---
+++ src/main/java/fr/epsi/gostyleapi/GostyleApiApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.gostyleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GostyleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GostyleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epsi/gostyleapi/GostyleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.gostyleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GostyleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
