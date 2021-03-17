29f268840e6bbad75d23742507836958995b7e8f
Thibault Doucet <thibault.doucet44@gmail.com>
2021-03-17 15:22:31 +0100

    :wrench: Updated servlet initializer

--- src/main/java/fr/epsi/gostyleapi/GostyleApiApplication.java
+++ src/main/java/fr/epsi/gostyleapi/GostyleApiApplication.java
@@ -2,12 +2,19 @@ package fr.epsi.gostyleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GostyleApiApplication {
+public class GostyleApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GostyleApiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(GostyleApiApplication.class);
 	}
 
 }
