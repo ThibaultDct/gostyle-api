fde2534ca5a766bb692fa9fa2d230f052790c145
Thibault Doucet <thibault.doucet44@gmail.com>
2021-03-17 15:24:48 +0100

    :wrench: Updated main class

--- src/main/java/fr/epsi/gostyleapi/GostyleApiApplication.java
+++ src/main/java/fr/epsi/gostyleapi/GostyleApiApplication.java
@@ -2,19 +2,12 @@ package fr.epsi.gostyleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GostyleApiApplication extends SpringBootServletInitializer {
+public class GostyleApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GostyleApiApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return builder.sources(GostyleApiApplication.class);
 	}
 
 }
