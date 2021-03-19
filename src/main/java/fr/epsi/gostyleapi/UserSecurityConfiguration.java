2b91dfa22bd1a0c21b199fd876e6e7339f85e085
Thibault Doucet <thibault.doucet44@gmail.com>
2021-03-19 13:39:30 +0100

    :sparkles: Allow POST method to /users without authentication for registering

--- src/main/java/fr/epsi/gostyleapi/UserSecurityConfiguration.java
+++ src/main/java/fr/epsi/gostyleapi/UserSecurityConfiguration.java
@@ -5,6 +5,7 @@ import fr.epsi.gostyleapi.services.auth.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -51,7 +52,7 @@ public class UserSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/authenticate").permitAll()
-                .antMatchers("/api-docs").permitAll()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
                 .anyRequest().authenticated().and()
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
